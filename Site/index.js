a4face8c7ed30b6c6b32b1da75f3b3d0864877d8
Augustin <augustin.viemont1@outlook.fr>
2026-01-27 12:25:39 +0100

    fixed docker compose

--- Site/index.js
+++ Site/index.js
@@ -4,9 +4,6 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import some_error from './API/middleware/Error.js';
 import pool from './API/Config/DataBase.js';
-import Application from './API/Routes/Applications.js';
-import Companies from './API/Routes/Companies.js';
-import Advertisement from './API/Routes/Advertisements.js';
 import People from './API/Routes/People.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -20,9 +17,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', People);
-app.use('/api', Application);
-app.use('/api', Companies);
-app.use('/api', Advertisement);
 
 app.use(express.static(path.join(__dirname, 'Front')));
 app.get('/', (req, res) => {
