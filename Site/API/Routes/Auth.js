447bea22cabae3b954b6f3e33b40aa0ca6496fd6
Augustin734 <augustin.viemont@epitech.com>
2026-02-07 10:45:31 +0100

    projet RTC présenté

--- Site/API/Routes/Auth.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { login } from "../Controllers/AuthControllers.js";
-
-const router = express.Router();
-
-router.post("/login", login);
-
-export default router;
